05c21a3d43b9d606ac49831296a91dd0f8b5da56
shivaraj-code <111412646+shivaraj-code@users.noreply.github.com>
2022-12-08 14:56:13 +0530

    Add files via upload
    first commit

---
+++ be-alpha/src/main/java/com/emr/dto/CountryRequest.java
@@ -0,0 +1,27 @@
+package com.emr.dto;
+
+import com.emr.model.Country;
+
+import lombok.ToString;
+
+@ToString
+public class CountryRequest {
+	
+	private Country country;
+
+	public Country getCountry() {
+		return country;
+	}
+
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+
+	public Object getbyId() {
+		// TODO Auto-generated method stub
+		return country.getCountry_Id() ;
+		
+	}
+	
+	
+}
